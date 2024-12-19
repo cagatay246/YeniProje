d8b6437504ea3c8e39a5f9ef0b52bc54e763901e
cagatay246 <cagataykilgi7@gmail.com>
2024-12-19 11:21:36 +0300

    Proje

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Proje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Bir sayı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            // 10 ile karşılaştırma yapma
+            if (sayi == 10)
+            {
+                Console.WriteLine($"{sayi}sayısı 10'a eşittir.");
+            }
+            else if (sayi < 10)
+            {
+                Console.WriteLine($"{sayi} sayısı 10'dan küçüktür.");
+            }
+            else
+            {
+                Console.WriteLine($"{sayi} sayısı 10'dan büyüktür.");
+            }
+
+            // Sayının çift mi, tek mi olduğunu kontrol etme
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine($"{sayi} sayısı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine($"{sayi} sayısı tektir.");
+
+
+            }
+        }
+    }
+}
